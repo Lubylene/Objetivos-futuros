53f88ef88520a8aa6c86033c6444c95781dda1af
Lubya <grando.lubya@escola.pr.gov.br>
2026-05-28 10:57:56 -0300

    cores

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ for(let i=0; i<botoes.length; i++){
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-06-14T00:00:00");
 const tempoObjetivo2 = new Date("2026-06-25T00:00:00");
-const tempoObjetivo3 = new Date("2026-12-180T00:00:00");
+const tempoObjetivo3 = new Date("2026-12-18T00:00:00");
 const tempoObjetivo4 = new Date("2027-02-03T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
